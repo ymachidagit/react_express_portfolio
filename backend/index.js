537c3adc03cf4d67cfa0bf11172cf2ec5c6f5490
ymachidagit <y.machida.1013@outlook.jp>
2022-09-08 03:21:38 +0900

    new commit

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,14 @@ const app = express()
 const path = require('path');
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello World!" });
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'../frontend/dist/index.html'));
+    res.sendFile(path.join(__dirname,'../frontend/build/index.html'));
 });
 
 app.listen(port, () => {
