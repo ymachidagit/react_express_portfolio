edbc68bfba38cbcfc2bb78b7c4e06ca63346bcbf
ymachidagit <y.machida.1013@outlook.jp>
2022-10-19 18:06:04 +0900

    各プロダクトページレイアウト修正

--- frontend/src/components/worksComponents/works/BIPROGYDXHACK.tsx
+++ frontend/src/components/worksComponents/works/BIPROGYDXHACK.tsx
@@ -13,7 +13,7 @@ function BIPROGYDXHACK() {
       <div className="container mx-auto flex flex-wrap p-10 flex-col bg-green-200">
         <div className="tab w-full overflow-hidden border-t">
           <input className="absolute opacity-0 " id="tab-multi-1" type="checkbox" name="tabs"/>
-          <label className="sm:text-xl text-xm font-medium title-font block  leading-normal cursor-pointer" htmlFor="tab-multi-1">開発の経緯</label>
+          <label className="sm:text-xl text-xm font-medium title-font block leading-normal cursor-pointer pr-3" htmlFor="tab-multi-1">開発の経緯</label>
           <div className="h-1 w-20 bg-indigo-500 rounded"></div>
           <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal mb-5">
             <p className="p-5">{work.motive}</p>
@@ -21,7 +21,7 @@ function BIPROGYDXHACK() {
         </div>
         <div className="tab w-full overflow-hidden border-t">
           <input className="absolute opacity-0 " id="tab-multi-2" type="checkbox" name="tabs"/>
-          <label className="sm:text-xl text-xm font-medium title-font block  leading-normal cursor-pointer" htmlFor="tab-multi-2">概要</label>
+          <label className="sm:text-xl text-xm font-medium title-font block  leading-normal cursor-pointer pr-3" htmlFor="tab-multi-2">概要</label>
           <div className="h-1 w-20 bg-indigo-500 rounded"></div>
           <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal mb-5">
             <p className="p-5">{work.abstract}</p>
@@ -29,7 +29,7 @@ function BIPROGYDXHACK() {
         </div>
         <div className="tab w-full overflow-hidden border-t">
           <input className="absolute opacity-0 " id="tab-multi-3" type="checkbox" name="tabs"/>
-          <label className="sm:text-xl text-xm font-medium title-font block  leading-normal cursor-pointer" htmlFor="tab-multi-3">開発の目的</label>
+          <label className="sm:text-xl text-xm font-medium title-font block  leading-normal cursor-pointer pr-3" htmlFor="tab-multi-3">開発の目的</label>
           <div className="h-1 w-20 bg-indigo-500 rounded"></div>
           <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal mb-5">
             <p className="p-5">{work.purpose}</p>
@@ -37,7 +37,7 @@ function BIPROGYDXHACK() {
         </div>
         <div className="tab w-full overflow-hidden border-t">
           <input className="absolute opacity-0 " id="tab-multi-4" type="checkbox" name="tabs"/>
-          <label className="sm:text-xl text-xm font-medium title-font block  leading-normal cursor-pointer" htmlFor="tab-multi-4">技術スタック</label>
+          <label className="sm:text-xl text-xm font-medium title-font block  leading-normal cursor-pointer pr-3" htmlFor="tab-multi-4">技術スタック</label>
           <div className="h-1 w-20 bg-indigo-500 rounded"></div>
           <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal mb-5">
             <p className="p-5">{work.techStack}</p>
@@ -45,7 +45,7 @@ function BIPROGYDXHACK() {
         </div>
         <div className="tab w-full overflow-hidden border-t">
           <input className="absolute opacity-0 " id="tab-multi-5" type="checkbox" name="tabs"/>
-          <label className="sm:text-xl text-xm font-medium title-font block  leading-normal cursor-pointer" htmlFor="tab-multi-5">自分の役割</label>
+          <label className="sm:text-xl text-xm font-medium title-font block  leading-normal cursor-pointer pr-3" htmlFor="tab-multi-5">自分の役割</label>
           <div className="h-1 w-20 bg-indigo-500 rounded"></div>
           <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal mb-5">
             <p className="p-5">{work.myRole}</p>
@@ -53,7 +53,7 @@ function BIPROGYDXHACK() {
         </div>
         <div className="tab w-full overflow-hidden border-t">
           <input className="absolute opacity-0 " id="tab-multi-6" type="checkbox" name="tabs"/>
-          <label className="sm:text-xl text-xm font-medium title-font block  leading-normal cursor-pointer" htmlFor="tab-multi-6">賞</label>
+          <label className="sm:text-xl text-xm font-medium title-font block  leading-normal cursor-pointer pr-3" htmlFor="tab-multi-6">賞</label>
           <div className="h-1 w-20 bg-indigo-500 rounded"></div>
           <div className="tab-content overflow-hidden border-l-2 bg-gray-100 border-indigo-500 leading-normal mb-5">
             <p className="p-5">{work.award}</p>
