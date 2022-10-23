e493915f39233af40697ea131ef790b87813991a
ymachidagit <y.machida.1013@outlook.jp>
2022-10-23 22:43:23 +0900

    worksページ修正

--- frontend/src/components/works.tsx
+++ frontend/src/components/works.tsx
@@ -21,9 +21,9 @@ function Works() {
       <Header></Header>
       <div className="container mx-auto flex flex-wrap p-10 flex-col bg-green-200">
         <div className="flex flex-wrap w-full">
-          <div className="lg:w-1/2 inline mb-6 lg:mb-0">
+          <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Works</h1>
-            <div className="h-1 w-full bg-indigo-500 rounded"></div>
+            <div className="h-1 w-20 bg-indigo-500 rounded"></div>
           </div>    
         </div>
         <p className="lg:w-1/2 w-full leading-relaxed flex flex-wrap mb-20 text-gray-500">研究 / インターン / ハッカソンで開発したアプリ一覧</p>
