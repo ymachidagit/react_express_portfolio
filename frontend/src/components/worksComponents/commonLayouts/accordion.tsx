62ba312dec1bce7f13b40922ad8e797d812d0630
ymachidagit <y.machida.1013@outlook.jp>
2022-12-04 00:09:13 +0900

    微調整

--- frontend/src/components/worksComponents/commonLayouts/accordion.tsx
+++ frontend/src/components/worksComponents/commonLayouts/accordion.tsx
@@ -9,7 +9,7 @@ export const Accordion = ({content}:{content:contentType}) => {
   return (
     <FlowbiteAccordion alwaysOpen={true}>
       <FlowbiteAccordion.Panel>
-        <FlowbiteAccordion.Title className="bg-green-300 hover:bg-green-100 sm:text-xl text-xm font-medium title-font cursor-pointer pr-3">
+        <FlowbiteAccordion.Title className="bg-green-300 hover:bg-green-200 sm:text-xl text-xm font-medium title-font cursor-pointer pr-3">
           {content.title}
         </FlowbiteAccordion.Title>
         <FlowbiteAccordion.Content>
